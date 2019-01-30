ba734eb4bb6fba6286d35ae39851b2d5c92a7eef
tongpisztt <pisztt@gmail.com>
2019-01-30 12:43:18 +0700

    add backend and frontend

---
+++ se-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule, Routes }  from '@angular/router';
+import { AgentAppointmentComponent } from './agent-appointment/agent-appointment.component';
+import { ShowAppointmentResultComponent } from './show-appointment-result/show-appointment-result.component';
+
+
+
+const routes: Routes = [
+{ path: 'agentAppointment', component: AgentAppointmentComponent },
+{ path: 'showAppointmentResult', component: ShowAppointmentResultComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
